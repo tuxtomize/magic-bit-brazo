3bf0ee1e41874247a5926b8e75fed6bd2f594cf2
Juan Antonio Cortés <jacortesm@gmail.com>
2020-10-26 07:28:49 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
 IrRemote.onPressEvent(RemoteButton.NUM9, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.White))
 })
-IrRemote.onPressEvent(RemoteButton.D, function () {
-    magicbit.Servo(magicbit.Servos.S7, 180)
-})
 IrRemote.onPressEvent(RemoteButton.Ok, function () {
+    magicbit.Servo(magicbit.Servos.S7, 105)
     tecla = 0
 })
 IrRemote.onPressEvent(RemoteButton.NUM0, function () {
@@ -16,9 +14,6 @@ IrRemote.onPressEvent(RemoteButton.NUM7, function () {
 IrRemote.onPressEvent(RemoteButton.NUM8, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Purple))
 })
-IrRemote.onPressEvent(RemoteButton.EQ, function () {
-    magicbit.Servo(magicbit.Servos.S8, 108)
-})
 IrRemote.onPressEvent(RemoteButton.NUM3, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
 })
@@ -28,12 +23,19 @@ IrRemote.onPressEvent(RemoteButton.NUM1, function () {
 IrRemote.onPressEvent(RemoteButton.UP, function () {
     tecla = 1
 })
+IrRemote.onPressEvent(RemoteButton.A, function () {
+    magicbit.Servo(magicbit.Servos.S8, 45)
+    basic.pause(1000)
+    magicbit.Servo(magicbit.Servos.S8, 108)
+})
 IrRemote.onPressEvent(RemoteButton.NUM6, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
 })
 IrRemote.onPressEvent(RemoteButton.Right, function () {
     tecla = 0
+    magicbit.Servo(magicbit.Servos.S7, 45)
     magicbit.StepperDegree(magicbit.Steppers.STPM1, 80)
+    magicbit.Servo(magicbit.Servos.S7, 105)
 })
 IrRemote.onPressEvent(RemoteButton.NUM5, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Blue))
@@ -43,19 +45,26 @@ IrRemote.onPressEvent(RemoteButton.NUM2, function () {
 })
 IrRemote.onPressEvent(RemoteButton.Left, function () {
     tecla = 0
+    magicbit.Servo(magicbit.Servos.S7, 165)
     magicbit.StepperDegree(magicbit.Steppers.STPM2, 80)
+    magicbit.Servo(magicbit.Servos.S7, 105)
 })
 IrRemote.onPressEvent(RemoteButton.B, function () {
-    magicbit.Servo(magicbit.Servos.S7, 110)
+    for (let index = 0; index < 4; index++) {
+        magicbit.Servo(magicbit.Servos.S8, 160)
+        basic.pause(1000)
+        magicbit.Servo(magicbit.Servos.S8, 60)
+        basic.pause(1000)
+    }
+    magicbit.Servo(magicbit.Servos.S8, 108)
 })
 IrRemote.onPressEvent(RemoteButton.NUM4, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Green))
 })
-IrRemote.onPressEvent(RemoteButton.Add, function () {
-    magicbit.Servo(magicbit.Servos.S8, 60)
-})
 IrRemote.onPressEvent(RemoteButton.C, function () {
-    magicbit.Servo(magicbit.Servos.S7, 130)
+    magicbit.Servo(magicbit.Servos.S8, 160)
+    basic.pause(1000)
+    magicbit.Servo(magicbit.Servos.S8, 108)
 })
 let tecla = 0
 let strip: neopixel.Strip = null
@@ -67,6 +76,7 @@ basic.pause(2000)
 basic.clearScreen()
 basic.forever(function () {
     if (tecla == 1) {
+        magicbit.Servo(magicbit.Servos.S7, 105)
         magicbit.StpCarMove(1, 30)
     }
 })
